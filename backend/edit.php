2eb661db23569061d385d399aa9a654b37dc81bd
DatRoyce <roycewhitaker@rocketmail.com>
2015-01-10 22:09:45 -0800

    Fixed Links Having Errors...

--- backend/edit.php
+++ backend/edit.php
@@ -27,8 +27,8 @@ $json = json_decode(file_get_contents("../meta/meta.txt"), true);
 				<li><a href="../<?php echo swd(strtolower($page)); ?>" target="_blank">View</a></li>
 			</ul>
 			<ul class="sec">
-				<li><a href="./new" title="New page">New</a></li>
-				<li><a href="./settings" title="Site settings">Settings</a></li>
+				<li><a href="./new.php" title="New page">New</a></li>
+				<li><a href="./settings.php" title="Site settings">Settings</a></li>
 				<li><a href="../" title="Visit site" target="_blank">Site</a></li>
 			</ul>
 		</nav>
