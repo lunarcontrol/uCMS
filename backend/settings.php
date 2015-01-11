a30ff119d147a7623a87d6424ea12fcf59c38fb6
DatRoyce <roycewhitaker@rocketmail.com>
2015-01-10 22:11:49 -0800

    Fixed Links Having Errors...

--- backend/settings.php
+++ backend/settings.php
@@ -18,8 +18,8 @@ $json = json_decode(file_get_contents("../meta/meta.txt"), true);
 				<li><a href="#" id="refresh" title="Refresh all pages">Refresh</a></li>
 			</ul>
 			<ul class="sec">
-				<li><a href="./new" title="New page">New</a></li>
-				<li><a href="./settings" title="Site settings">Settings</a></li>
+				<li><a href="./new.php" title="New page">New</a></li>
+				<li><a href="./settings.php" title="Site settings">Settings</a></li>
 				<li><a href="../" title="Visit site" target="_blank">Site</a></li>
 			</ul>
 		</nav>
